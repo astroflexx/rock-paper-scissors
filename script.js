44fecab1ac04b454fe98c175757ccdaa73e3a073
Adithya V <venkateshadithya007@gmail.com>
2024-01-18 00:15:37 +0530

    Implement UI for Rock Paper Scissors Game

--- script.js
+++ script.js
@@ -1,37 +1,74 @@
 let arr = ['rock', 'paper', 'scissors'];
 
+let rock = document.querySelector('.rock');
+let paper = document.querySelector('.paper');
+let scissors = document.querySelector('.scissors');
+
+let playerScore = document.querySelector('.player-score');
+let computerScore = document.querySelector('.computer-score');
+
+function handleRock() {
+    playRound('rock', getComputerChoice());
+}
+
+function handlePaper() {
+    playRound('paper', getComputerChoice());
+}
+
+function handleScissors() {
+    playRound('scissors', getComputerChoice());
+}
+
+rock.addEventListener('click', handleRock);
+paper.addEventListener('click', handlePaper);
+scissors.addEventListener('click', handleScissors);
+
+let roundResult = document.createElement('p');
+document.body.appendChild(roundResult);
+
+let overallResult = document.createElement('h1');
+document.body.appendChild(overallResult);
+
 function getComputerChoice() {
     return arr[Math.floor(Math.random() * 3)];
 }
 
-function getPlayerChoice() {
-    return window.prompt('Enter your choice');
-}
-
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return `Its a tie!`;
+        roundResult.textContent = `This round is a draw. You chose ${playerSelection} and computer chose ${computerSelection}.`;
     }
-
+    
     else if (
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper')
     ) {
-        return `You Win! You chose ${playerSelection} and computer chose ${computerSelection}`;
+        playerScore.textContent = parseInt(playerScore.textContent) + 1;
+        roundResult.textContent = `You win this round! You chose ${playerSelection} and computer chose ${computerSelection}.`;
     }
 
     else {
-        return `You Lose! You chose ${playerSelection} and computer chose ${computerSelection}`;
+        computerScore.textContent = parseInt(computerScore.textContent) + 1;
+        roundResult.textContent = `You lose this round. You chose ${playerSelection} and computer chose ${computerSelection}.`;
+    }
+
+    checkWinner();
+}
+
+function checkWinner() {
+    if (parseInt(playerScore.textContent) === 5) {
+        overallResult.textContent = `The game has ended. You Win ${playerScore.textContent} to ${computerScore.textContent}! Refresh the page to play again.`;
+        end();
+    }
+
+    else if (parseInt(computerScore.textContent) === 5) {
+        overallResult.textContent = `The game has ended. You Lose ${playerScore.textContent} to ${computerScore.textContent}. Refresh the page to play again.`;
+        end();
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
-
-game();
+function end() {
+    rock.removeEventListener('click', handleRock);
+    paper.removeEventListener('click', handlePaper);
+    scissors.removeEventListener('click', handleScissors);
+} 
